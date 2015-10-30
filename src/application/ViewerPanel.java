9cf3e6c3ce4e8c890010122cd4685e96fb909585
JhaoWeiHuang <jhaowei.huang@gmail.com>
2015-10-30 13:35:23 +0800

    init

---
+++ src/application/ViewerPanel.java
@@ -0,0 +1,15 @@
+package application;
+
+import edu.ufl.digitalworlds.opengl.OpenGLPanel;
+
+public class ViewerPanel extends OpenGLPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void draw() {
+		
+	}
+}
