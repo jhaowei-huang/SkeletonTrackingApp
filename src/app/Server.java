6844543f37a22b582736ab8d3812190377d8b08e
JhaoWeiHuang <jhaowei.huang@gmail.com>
2015-11-20 15:29:55 +0800

    rename CoAPServer to Server

---
+++ src/app/Server.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.net.SocketException;
+
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+
+public class Server extends CoapServer {
+	public Server() throws SocketException {
+		// provide an instance of a SkeletonTracking resource
+        add(new Root("sensor"));
+        add(new Root("actuator"));
+	}
+}
