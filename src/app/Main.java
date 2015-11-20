deadf280367b43928b25b904cb91aa2f7aec532a
JhaoWeiHuang <jhaowei.huang@gmail.com>
2015-11-20 15:34:24 +0800

    implement observing resources in CoAP

--- src/app/Main.java
+++ src/app/Main.java
@@ -17,9 +17,10 @@ import edu.ufl.digitalworlds.j4k.J4KSDK;
 public class Main extends Application {
 	private final Box box = new Box(200, 200, 200);
 	private final Text text = new Text(200, 200, "#current");
-	protected static Kinect kinect = null;
+	protected Kinect kinect = null;
 	private Timer timer = new Timer();
 	protected static DetectSkeletonsTask detectSkeletonsTask; // = new DetectSkeletonsTask();
+	private Server server;
 	
 	public Main() {
 		kinect = new Kinect();
@@ -27,11 +28,11 @@ public class Main extends Application {
 		kinect.start(J4KSDK.SKELETON);
 		kinect.initSkeletons();
 		detectSkeletonsTask = new DetectSkeletonsTask();
-		timer.schedule(detectSkeletonsTask, 100, 3000);
+		timer.schedule(detectSkeletonsTask, 100, 1000);
 		
 		try {
             // create server
-            CoAPServer server = new CoAPServer();
+            server = new Server();
             server.start();
             System.out.println("coap server start!");
         } catch (SocketException e) {
@@ -60,22 +61,52 @@ public class Main extends Application {
 	class DetectSkeletonsTask extends TimerTask {
 		private int previousEventCounts = 0;
 		private String result = "no person";
+		private int peopleCount = 0;
 		@Override
 		public void run() {
-			int eventCounts = Kinect.getSkeletonsEventCounts();
+			if(kinect == null)
+				return;
+			
+			int eventCounts = kinect.getSkeletonsEventCounts();
 			String msg = "event counts = " + eventCounts;
-			
-			if(previousEventCounts == eventCounts)
-				result = msg + " -> no person";
-			else
+			// no person
+			if(previousEventCounts == eventCounts) {
 				result = msg + " -> current = " + Kinect.getCurrent();
-			
+				if(kinect.getCurrent() != 0) {
+					peopleCount = 0;
+					kinect.setCurrent(0);
+					observePeopleCount();
+				}
+			}
+			// current people count > 0
+			else {
+				result = msg + " -> current = " + Kinect.getCurrent();
+				// people count changed
+				if(peopleCount != kinect.getCurrent()) {
+					observePeopleCount();
+					peopleCount = kinect.getCurrent();
+				}
+			}
+
 			System.out.println(result);
 			previousEventCounts = eventCounts;
 		}
 		
-		public String getResult() {
-			return result;
+		public boolean observePeopleCount() {
+			if(server != null) {
+				SkeletonTrackingResource skr = (SkeletonTrackingResource) server.getRoot().getChild("sensor").getChild("peopleCount");
+				if(skr != null) {
+					System.out.println("observe " + skr.getName());
+					skr.peopleCountChanged();
+				}
+				else return false;
+			}
+			else {
+				System.out.println("no server");
+				return false;
+			}
+			
+			return true;
 		}
 	}
 }
--- src/app/SkeletonTrackingResource.java
+++ src/app/SkeletonTrackingResource.java
@@ -7,15 +7,19 @@ import edu.ufl.digitalworlds.j4k.Skeleton;
 
 public class SkeletonTrackingResource extends CoapResource {
 	public SkeletonTrackingResource() {
-		super("skeletontracking");
-        // set display name
-        getAttributes().setTitle("skeletontrackingapp");
+		super("peopleCount");
+		this.setObservable(true);
+	}
+	
+	public void peopleCountChanged() {
+		System.out.println("observe");
+		this.changed();
 	}
 	
 	@Override
     public void handleGET(CoapExchange exchange) {
         // respond to the request
 		
-		exchange.respond(Main.detectSkeletonsTask.getResult());
+		exchange.respond("" + Kinect.getCurrent());
     }
 }
